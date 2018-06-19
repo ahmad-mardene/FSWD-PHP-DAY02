1128beb15f49fcd86ccac94000d6415b2b3717be
ahmad-mardene <31830661+ahmad-mardene@users.noreply.github.com>
2018-06-19 11:34:34 +0200

    Add files via upload

---
+++ ex3.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+       <title>Exercise 3</title>
+</head>
+<body>
+
+<?php
+
+function devideFunction($num1, $num2)
+{
+$dev = $num1 / $num2;
+echo " devision result of the two numbers is : $dev";
+}
+devideFunction(20, 5);
+?>
+</body>
+</html>
